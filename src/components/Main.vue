5fe2711b1041c33e05beb4a9e7718ad5eec6a9d1
Frederick Wang <hwoam@outlook.com>
2017-06-15 19:00:12 +0800

    A lot of bugs fixed.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,6 +2,12 @@
   <div class="main">
     <Row class="headline">
       <Col span="24">
+      <Alert v-if="in_wechat_or_qq" type="warning" show-icon closable>
+        <strong>TXT文件不能正常下载？</strong>
+        <span slot="desc">
+          <strong>安卓版微信与QQ的内置浏览器</strong>比较落后，如果您无法正常下载txt文件，请点击右上角的省略号按钮，并选
+          <strong>“用浏览器打开”</strong>，再在新打开的浏览器页面中进行下载。</span>
+      </Alert>
       <h1>百度文库文档转文字工具</h1>
       <p>版本号：1.0.0 | 最后更新：2017-6-15</p>
       <p>Powered by
@@ -17,7 +23,7 @@
       <br/>
       <Button type="success" @click="start_conversion">开始转换</Button>
       <Button type="error" @click="reset">重置</Button>
-      <Button type="info" :disabled="save_btn_disabled" :loading="save_btn_loading" @click="save_txt">保存txt</Button>
+      <Button type="info" :disabled="save_btn_disabled" :loading="save_btn_loading" @click="save_txt">下载txt</Button>
       </Col>
     </Row>
     <Row class="conversion-result">
@@ -49,13 +55,16 @@ export default {
   },
   methods: {
     start_conversion: function () {
+      if (this.document_address === '') {
+        return
+      }
       this.clear_result()
       // 简单的处理。把电脑版的网址转化为Wap版的网址
       let wapVersionAddress = this.document_address.split('?')[0]
       if (wapVersionAddress.indexOf('wapwenku.baidu.com') === -1) {
         wapVersionAddress = wapVersionAddress.replace(/wenku.baidu.com/, 'wapwenku.baidu.com')
       }
-      window.$.get(`http://${window.location.hostname}/proxy.php`, { address: wapVersionAddress }, (body) => {
+      window.$.get(`http://frederickwang.com/code/baidu_wenku_text_converter/proxy.php`, { address: wapVersionAddress }, (body) => {
         this.save_btn_disabled = false
         this.save_btn_loading = true
         //
@@ -70,7 +79,7 @@ export default {
         })
         currentPage++
         var fetchPage = () => {
-          window.$.get(`http://${window.location.hostname}/proxy.php`, { address: `${wapVersionAddress}?pn=${currentPage}&pu=` }, (body) => {
+          window.$.get(`http://frederickwang.com/code/baidu_wenku_text_converter/proxy.php`, { address: `${wapVersionAddress}?pn=${currentPage}&pu=` }, (body) => {
             let newPage = window.$('<div></div>').html(body)
             window.$('.content', newPage).find('.txt').each((index, element) => {
               let txt = String(window.$(element).text()).trim()
@@ -107,6 +116,15 @@ export default {
       }
       let blob = new Blob([text], { type: 'text/plain;charset=utf-8' })
       FileSaver.saveAs(blob, `${this.title}.txt`)
+    }
+  },
+  computed: {
+    in_wechat_or_qq: function () {
+      if (navigator.userAgent.toLowerCase().indexOf('micromessenger') > -1 || navigator.userAgent.toLowerCase().indexOf('qq') > -1) {
+        return true
+      } else {
+        return false
+      }
     }
   },
   components: {
